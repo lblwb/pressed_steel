cd9f4f3b5c44611f3ad6e8faba1fe9e37ea93165
Aleksey <alexsawenkov@gmail.com>
2025-06-09 19:04:21 +0300

    fix - page special

--- patterns/sales-specials-block.php
+++ patterns/sales-specials-block.php
@@ -6,30 +6,48 @@
 ?>
 <?php
 
-if (have_rows('sales')):
-while (have_rows('sales')) :
-the_row();
-
-//        $dataPlatform = get_sub_field('platform');
+//var_dump(have_rows('special'));
+//
+//if (have_rows('special')):
+//while (have_rows('special')) :
+//the_row();
+//
+////        $dataPlatform = get_sub_field('platform');
 ?>
-<div class="pageSpecialBodyList">
+
+
+<?php
+// Query specials
+$query = new WP_Query(array(
+    'post_type' => 'special',
+    'posts_per_page' => -1,
+    'orderby' => 'date',
+    'order' => 'DESC',
+));?>
+
+<div class="pageSpecialBodyList" style="padding: 100px 0;">
+    <?php if( $query->have_posts() ): ?>
+        <?php while( $query->have_posts() ): $query->the_post(); ?>
     <div class="pageSpecialBodyListItem">
         <div class="featuresBlockCol">
             <div class="featuresBlockHeadingWrap" style="display: flex; justify-content: space-between">
-                <div class="featuresBlockHeading" style="margin-bottom: 4rem;min-width: 28.8vw;">
-                    <div class="featuresBlockHeadingTitle">
-                        <span class="headingTitle __HighActive">01</span>
-                        <span><?php echo get_the_title() ?></span>
+                <div class="featuresBlockHeading" style="width: 100%">
+                    <div class="featuresBlockHeadingTitle" style="display: flex; width: 100%;">
+                        <span class="headingTitle __HighActive" style="flex: 0;">01</span>
+                        <span style="flex: auto; text-align: center; width: 100%"><?php echo get_the_title() ?></span>
                     </div>
                 </div>
             </div>
 
             <div class="featuresBlockBody" style="color: #000;">
+                <svg width="1400" height="420" viewBox="0 0 1400 420" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%;">
+                    <rect width="1400" height="420" fill="#E9ECF5"/>
+                </svg>
             </div>
         </div>
-        <?php
-        endwhile;
-        endif;
-        ?>
     </div>
+        <?php endwhile; wp_reset_postdata(); ?>
+    <?php else: ?>
+        <p><?php _e('No specials found.', 'textdomain'); ?></p>
+    <?php endif; ?>
 </div>
