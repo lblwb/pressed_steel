b3a7180de5149c176818d04a1848cd3c542947b7
Aleksey <alexsawenkov@gmail.com>
2025-06-19 16:06:44 +0300

    sales special block -> list fix post

--- patterns/sales-specials-block.php
+++ patterns/sales-specials-block.php
@@ -31,20 +31,25 @@ $query = new WP_Query(array(
 
 <section class="featuresBlock">
     <div class="featuresBlockWrapper gridWrap">
-
         <?php if ($query->have_posts()): ?>
             <?php while ($query->have_posts()): $query->the_post(); ?>
+                <?php
+                $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                if (!$thumbnail_url) {
+                    $thumbnail_url = get_theme_file_uri('assets/images/prdctImage2Bg.png'); // fallback
+                }
+                ?>
                 <article class="featuresBlockCol">
                     <header class="featuresBlockHeadingWrap">
                         <div class="featuresBlockHeading">
                             <h2 class="featuresBlockHeadingTitle">
                                 <span class="headingTitle __HighActive"><?php echo str_pad($i++, 2, '0', STR_PAD_LEFT); ?></span>
-                                <!--                    --><?php //echo esc_html(get_the_title()); ?>
+                                                    <?php echo esc_html(get_the_title()); ?>
                             </h2>
                         </div>
                         <div class="featuresBlockBodyHeading __Table __Main __Mob">
                             <h3 class="featuresBlockBodyHeadingTitle">
-                                <?php echo esc_html(get_the_title()); ?>
+                                <?php // echo esc_html(get_the_title()); ?>
                             </h3>
                             <span class="featuresBlockBodyHeadingDesc"><?php echo wp_kses_post(get_the_content()); ?></span>
                         </div>
@@ -54,17 +59,17 @@ $query = new WP_Query(array(
                             <div class="featuresBlockBodyLt">
                                 <div class="featuresBlockMbImg">
                                     <img class="featuresBlockBodyFiImg"
-                                         src="<?php echo esc_url(get_theme_file_uri('assets/images/prdctImage2Bg.png')); ?>"
-                                         alt="Решетчатый настил PressedSteel" loading="lazy">
+                                         src="<?php echo esc_url($thumbnail_url); ?>"
+                                         alt="<?php the_title_attribute(); ?>" loading="lazy">
                                 </div>
                                 <div class="featuresBlockMoreBtn">
                                 </div>
                             </div>
                             <div class="featuresBlockBodyRt">
                                 <div class="featuresBlockBodyFi">
-                                    <img class="featuresBlockBodyFiImg"
-                                         src="<?php echo esc_url(get_theme_file_uri('assets/images/prdctImage2Bg.png')); ?>"
-                                         alt="Решетчатый настил PressedSteel" loading="lazy">
+                                    <img class="featuresBlockBodyFiImg" style="max-height: 450px;width: 100%;object-fit: cover;object-position: center;"
+                                         src="<?php echo esc_url($thumbnail_url); ?>"
+                                         alt="<?php the_title_attribute(); ?>" loading="lazy">
                                 </div>
                             </div>
                         </div>
