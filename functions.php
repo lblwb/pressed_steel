04ec8a761492f1599e7d22e9ab1d287518d87c8b
Aleksey <alexsawenkov@gmail.com>
2025-07-03 15:35:50 +0300

    yandex metrix -> delete

--- functions.php
+++ functions.php
@@ -183,24 +183,6 @@ function pst_init_woocommerce()
 
 add_action('init', 'pst_init_woocommerce');
 
-function add_yandex_metrika() {
-    if ( ! is_admin() ) {
-        ?>
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};m[i].l=1*new Date();for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(103163798, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true
-        });
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/103163798" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
-    <?php
-    }
-}
-add_action('wp_footer', 'add_yandex_metrika');
-
 require_once(__DIR__ . "/inc/mobile_full_menu.php");
 require_once(__DIR__ . "/inc/product_params.php");
 require_once(__DIR__ . "/inc/action_form_cta.php");
