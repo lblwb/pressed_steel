55cc10e0f7dbb420160d91c773166156e3254e14
Aleksey <alexsawenkov@gmail.com>
2025-07-03 15:31:51 +0300

    fix -> zone vision -> yandex metrix -> 2

--- functions.php
+++ functions.php
@@ -188,13 +188,7 @@ function add_yandex_metrika() {
         ?>
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(103163798, "init", {
+        (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};m[i].l=1*new Date();for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(103163798, "init", {
             clickmap:true,
             trackLinks:true,
             accurateTrackBounce:true
@@ -203,6 +197,7 @@ function add_yandex_metrika() {
     <noscript><div><img src="https://mc.yandex.ru/watch/103163798" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
     <?php
+    }
 }
 add_action('wp_footer', 'add_yandex_metrika');
 
